f4cca2a6e1011e553f7817e6cfa4cb1846769ae4
Honest <TMAM9@unigroupinc.com>
2022-09-12 20:45:04 -0500

    Challenge Code

---
+++ MechaCarChallenge.R
@@ -0,0 +1,3 @@
+mecha_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg))
